b3b024b772d03c9067828f9541a8ce6e0cdb289e
Sabri Bin Saep <sabrisaep@gmail.com>
2023-09-15 16:04:17 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+x = 73
+
+
+def uji_umur(umur):
+    if umur > 60:
+        print('Warga emas')
+    else:
+        print('Masih muda', x)
+
+
+def kereta():
+    y = 'Honda'
+    print(f'{y[1]}')
+
+
+if __name__ == '__main__':
+    # uji_umur(64)
+    kereta()
